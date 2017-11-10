8a10892c2cd18108002c89e17ddd4cda3ed3f43d
Pavel <mac@enters-iMac.local>
2017-11-10 15:53:28 +0200

    upload

---
+++ dev/static/js/main.js
@@ -0,0 +1,7 @@
+var contact = $('.contact');
+var contactPopup = $('.contact__popup');
+
+contact.on('click', function () {
+  contactPopup.addClass('contact__popup--vissible');
+});
+//# sourceMappingURL=main.js.map
---
+++ markup/static/js/libraries/jq-code.js
@@ -0,0 +1,6 @@
+var contact = $('.contact');
+var contactPopup = $('.contact__popup');
+
+contact.on('click', function () {
+  contactPopup.addClass('contact__popup--vissible');
+});
